51af5cba3f8995c0cba32232b5867578049f1ea5
Nicholas Ketchum <nketchum@gmail.com>
2017-06-20 16:44:56 -0400

    save fid instead of file url and show existing image if present

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -5,6 +5,7 @@ namespace Drupal\custom_design\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
 
 /**
  * Defines a form that configures custom_design settings.
@@ -32,12 +33,22 @@ class SettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
     $custom_design_config = $this->config('custom_design.settings');
+    $config = \Drupal::service('config.factory')->getEditable('custom_design.settings');
+
+    $description = t('Upload an image file. Available extensions (.png .jpg .jpeg)');
+
+    if ($fid = $config->get('fid')) {
+      $file = File::load($fid);
+      $uri = $file->getFileUri();
+      $style = \Drupal::entityTypeManager()->getStorage('image_style')->load('thumbnail');
+      $description .= '<p><strong>Existing image:</strong><br><img src="'. $style->buildUrl($uri) . '"></p>';
+    }
 
     $form['bg_image_file'] = array(
       '#title' => t('Background image'),
       '#type' => 'managed_file',
       '#upload_location' => 'public://',
-      '#description' => t('Upload an image file. Available extensions (.png .jpg .jpeg)'),
+      '#description' => $description,
       '#upload_validators' => array(
         'file_validate_extensions' => array('png jpg jpeg'),
       ),
@@ -57,9 +68,13 @@ class SettingsForm extends ConfigFormBase {
       if (is_array($values['bg_image_file'])) {
         $fid = $values['bg_image_file'][0];
         $file = \Drupal\file\Entity\File::load($fid);
+        $file->setPermanent();
+        $file_usage = \Drupal::service('file.usage');
+        $file_usage->add($file, 'custom_design', 'custom_design', \Drupal::currentUser()->id());
+
         if ($file->url()) {
           $this->config('custom_design.settings')
-            ->set('bg_image_url', $file->url())
+            ->set('fid', $fid)
             ->save();
         }
       }
